ff5f91c6fdb70bb8f7ec71082987145c02b082dc
dipil-saud <dipil.saud@sprout-technology.com>
2011-04-20 17:15:20 +0545

    Mail Setup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "sprout.notes@sprout-technology.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ SproutNotes::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'notes.sprout-technology.com' }
+
 end
